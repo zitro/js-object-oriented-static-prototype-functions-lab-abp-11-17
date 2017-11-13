5b91886a798b794e5c51afc648327190f95ceceb
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-13 03:31:42 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,10 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+return emails.map(function(email){
+  return new IcebreakerResponse(email)
+})
+
+
+}
